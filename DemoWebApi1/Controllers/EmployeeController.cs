a2e68e8610769184623b5dde68958eb0914669ab
4QT-T-03 <4QT-T-03@dc-serv.4qt.com>
2024-06-01 18:51:14 +0530

    v

---
+++ DemoWebApi1/Controllers/EmployeeController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata;
+using System.Text.Json;
+using DemoWebApi1.Models;
+using Newtonsoft.Json;
+
+namespace DemoWebApi1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        /*
+      public static List<Employee> Employeelist1 = new List<Employee> {
+
+           new Employee { Id=1,Name="Rahul Gupta",City="Delhi",IsActive=true},
+           new Employee { Id=2,Name="Samaira Goel",City="Noida",IsActive=true},
+           new Employee { Id=3,Name="Rohit Yadav",City="Pune",IsActive=false},
+           new Employee { Id=4,Name="Pulak Sahu",City="Mumbai",IsActive=true}
+
+      };
+        */
+
+
+        static List<Employee> Employeelist = new ListObject().GetEmployees();
+
+
+        [HttpGet]
+        public List<Employee> Get()
+        {
+            
+            return Employeelist;
+        }
+
+        [HttpGet("{id}")]
+        public Employee Get(int id)
+        {
+            Employee getemployee = Employeelist.Find(e => e.Id == id);
+
+            if (getemployee != null)
+            {
+                return getemployee;
+            
+            }
+            
+            return getemployee;
+           // return "No data Found";
+        }
+        
+        [HttpPost("{id},{name},{city}")]
+        public List<Employee> Post(int id,string name,string city)
+        {
+            Employee e = new Employee { Id = id, Name = name, City=city,IsActive=true};
+            Employeelist.Add(e);
+            return Employeelist;
+        }
+
+        [HttpPut("{id}")]
+        public List<Employee> Put(int id, string name, string city)
+        {
+
+
+           
+            Employee updatedemployee = Employeelist.Find(e => e.Id == id);
+            
+            
+            if(updatedemployee != null)
+            {
+                updatedemployee.Name = name;
+                updatedemployee.City = city;
+            }
+            
+            return Employeelist;
+        }
+
+        [HttpDelete("{id}")]
+        public List<Employee> Delete(int id)
+        {
+
+            Employee deleteemployee = Employeelist.Find(e => e.Id == id);
+
+            if (deleteemployee != null)
+            {
+                Employeelist.Remove(deleteemployee);
+            }
+            
+            return Employeelist;
+        }
+
+    }
+    
+}
---
+++ DemoWebApi1/Models/Employee.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Text.Json;
+namespace DemoWebApi1.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public bool IsActive { get; set; } = true;
+
+
+    }
+    
+    public class ListObject
+    {
+        
+        public List<Employee> GetEmployees()
+        {
+           
+
+            string jsonText = File.ReadAllText(@"D:\Ritwik\DemoWebApi1\DemoWebApi1\data.json");
+
+            
+            List<Employee> Employeelist = JsonConvert.DeserializeObject<List<Employee>>(jsonText);
+           
+
+            return Employeelist;
+        }
+
+    }
+
+}
